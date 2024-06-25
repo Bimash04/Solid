9d23770390cbcb373eaff269ab76d7ca46997ddc
bimash dulal <bimashdulal7890@gmail.com>
2024-06-25 15:58:23 +0545

    2

---
+++ test.py
@@ -0,0 +1,29 @@
+# a = int (input ("enter the number :"))
+# b= int (input ("enter the another number :"))
+
+
+# def add(a,b):
+#     print (a+b)
+# def subtract(a,b):
+#     print (a-b)
+# def divide(a,b):
+#     print (a/b)
+# def multiply(a,b):
+#     print (a*b)
+
+# add(a,b)
+# subtract(a,b)
+# divide(a,b)
+# multiply(a,b)
+
+
+
+#  creation of replictions
+n = int(input ("enter the number of operations:"))
+count =0
+while count < n:
+    
+    a=(input("enter the number"))
+    b=eval(a)
+    print (b)
+    count += 1
